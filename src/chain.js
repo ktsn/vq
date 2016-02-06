b1efa119cce7e6d14c7315221282402df6810f85
ktsn <ktsn55@gmail.com>
2016-02-06 15:00:17 +0900

    Add helper functions to update velocity options

--- src/chain.js
+++ src/chain.js
@@ -7,9 +7,34 @@ const helpers = {
     return this;
   },
 
+  duration(msec) {
+    this._opts.duration = msec;
+    return this;
+  },
+
+  easing(name) {
+    this._opts.easing = name;
+    return this;
+  },
+
   progress(fn, tween = null) {
     this._props.tween = tween || this._props.tween || [1, 0];
     this._opts.progress = fn;
+    return this;
+  },
+
+  display(value) {
+    this._opts.display = value;
+    return this;
+  },
+
+  visibility(value) {
+    this._opts.visibility = value;
+    return this;
+  },
+
+  loop(count) {
+    this._opts.loop = count;
     return this;
   }
 };
